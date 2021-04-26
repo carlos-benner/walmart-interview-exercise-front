070a2d43d6ea094c017af654e96d9b77ac9f950d
Carlos Benner <carlos.benner.b@gmail.com>
2021-04-26 13:40:38 -0400

    Search product by id

--- routes/products.js
+++ routes/products.js
@@ -5,14 +5,26 @@ const api = express.Router();
 
 api.get('/:query', (req, res) => {
     const apiUrl = process.env.BACKEND_URL || 'http://localhost:3000';
-    axios
-        .get(`${apiUrl}/products/search/${req.params.query}`)
-        .then((result) => {
-            res.json(result.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+
+    if (req.params.query && !isNaN(req.params.query) && req.params.query > 0) {
+        axios
+            .get(`${apiUrl}/products/${req.params.query}`)
+            .then((result) => {
+                return res.json([result.data]);
+            })
+            .catch((err) => {
+                return res.status(err.response.status).json(err.response.data);
+            });
+    } else {
+        axios
+            .get(`${apiUrl}/products/search/${req.params.query}`)
+            .then((result) => {
+                return res.json(result.data);
+            })
+            .catch((err) => {
+                return res.status(err.response.status).json(err.response.data);
+            });
+    }
 });
 
 module.exports = api;
