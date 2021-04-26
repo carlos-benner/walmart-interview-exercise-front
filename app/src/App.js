b87461d2e745ebe411a40b385b641c4395c9540d
Carlos Benner <carlos.benner.b@gmail.com>
2021-04-26 10:31:36 -0400

    Installing axios and fetching products by calling an endpoint placeholder

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,37 @@
 import Header from './components/Header';
+import axios from 'axios';
 import './App.css';
 
-function onSearchSubmit() {
-    //TODO: create function to fetch data
-}
+import { useState } from 'react';
+
+const productsArray = [
+    {
+        id: 1,
+        brand: 'Samsung',
+        description: 'Plasma tv',
+        image: './lider-logo.svg',
+        price: 1000,
+        original_price: 2000,
+        discount: 50,
+    },
+];
 
 function App() {
+    const [productList, setProductList] = useState([]);
+
+    function onSearchSubmit(ev, query) {
+        ev.preventDefault();
+        setProductList(productsArray);
+        console.log(productList);
+        //axios(`/api/product/${query}`)
+        //   .then((result) => {
+        //       setProductList(result);
+        //       console.log(productList);
+        //   })
+        //   .catch((err) => {
+        //       console.log(err);
+        //   });
+    }
     return (
         <div className='App'>
             <Header searchFunction={onSearchSubmit}></Header>
